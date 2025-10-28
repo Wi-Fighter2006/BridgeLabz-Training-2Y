729f51095813aa46ad34618c1911352121008f12
Piyush Choudhary <piyushchudhary97@gmail.com>
2025-10-28 11:07:56 +0530

    Deleted

--- FunctionalInterface/BackgroundJob.java
+++
@@ -1,8 +0,0 @@
-package FunctionalInterface;
-
-public class BackgroundJob {
-    public static void main(String[] args) {
-        Runnable job = () -> System.out.println("Background job running asynchronously...");
-        new Thread(job).start();
-    }
-}
